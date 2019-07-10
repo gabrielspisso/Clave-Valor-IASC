a8ad160f2e75a712d4953ebcfc0876ae557293cc
lclauser <lclauser@est.frba.utn.edu.ar>
2019-07-09 21:34:19 -0300

    fix tabs

--- Supervisor/init/sockets.js
+++ Supervisor/init/sockets.js
@@ -2,11 +2,11 @@ const socketIO = require("socket.io");
 const supervisor = require("../model/supervisor");
 
 module.exports = server => {
-  	const io = socketIO(server);
-  	io.on("connection", socket => {
-  		supervisor.add(socket);
-    	socket.on("disconnect", () => {
- 	 		supervisor.remove(socket);
-    	});
-  	});
+	const io = socketIO(server);
+	io.on("connection", socket => {
+		supervisor.add(socket);
+		socket.on("disconnect", () => {
+			supervisor.remove(socket);
+		});
+	});
 }
