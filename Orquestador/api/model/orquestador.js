b2cafeed3783f999391abefdb6b795adc44f9109
gabrielspisso <gabrielhspisso@gmail.com>
2019-07-14 13:32:52 -0300

    elige nodo a ppartir de la clave

--- Orquestador/api/model/orquestador.js
+++ Orquestador/api/model/orquestador.js
@@ -2,10 +2,11 @@ const _ = require("lodash");
 const Promise = require("bluebird");
 const request = require("request-promise");
 const config = require("../../config");
+const Id = require('dht-id');
 
 class Orquestador{
     constructor(){
-        this.datos = _.map(config.datos, function(dir){ 
+        this.nodes = _.map(config.nodes, function(dir){ 
             return { 
                 ip: dir, 
                 active: true 
@@ -13,6 +14,22 @@ class Orquestador{
         });
     }
 
-    
+    getNodeByKey(key){
+        var idNode = this.getId(key);
+        return (this.nodes[idNode].active) ? this.nodes[idNode] : this.nextNode(idNode);
+    }
 
+    getId(key){
+        var hashId = new Id(key).toDec();
+        var resto = hashId % this.nodes.length;
+        var cociente = Math.floor(hashId / this.nodes.length);
+        return (resto == 0) ? cociente -1 : resto -1;
+    }
+
+    nextNode(idNode){//Si el proximo nodo esta activo trae ese, sino busca uno activo
+        return (idNode<this.nodes.length-1 && this.nodes[idNode+1].active) ?
+                this.nodes[idNode+1] :
+                this.nodes.find( (node) => node.active);
+    }
+    
 }
