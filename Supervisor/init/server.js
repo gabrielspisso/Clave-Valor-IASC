07e9be746ab0a672c7bb065254956248da72ac20
Diego <diegoperezferrando@gmail.com>
2019-07-14 19:18:15 -0300

    Algunos fixes de errores falopa en supervisor

--- Supervisor/init/server.js
+++ Supervisor/init/server.js
@@ -1,4 +1,5 @@
 const app = require("express")();
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.SUPERVISORPORT || 9999;
+console.log("SERVER LISTENING ON ", PORT)
 
 module.exports = app.listen(PORT);
