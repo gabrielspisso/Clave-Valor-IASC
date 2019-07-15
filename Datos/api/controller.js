db08259481e1837feb363bdb1906826f4fd027e4
SantiagoParedes97 <sparedes@est.frba.utn.edu.ar>
2019-07-15 00:36:04 -0300

    Ahora en el get tambien se hace con promise

--- Datos/api/controller.js
+++ Datos/api/controller.js
@@ -1,27 +1,25 @@
 const RepositorioDeDatos = require('./model/repositorioDeDatos');
 const BodyInvalido = require('./model/exceptions/BodyInvalido');
+const NotFound = require('./model/exceptions/NotFound');
+
 const Promise = require("bluebird");
 const _ = require("lodash");
 
 class Controller {
     constructor() {
         this.repositorioDeDatos = new RepositorioDeDatos();
-        _.bindAll(this, ["escribirValor", "obtenerValorDeClave", "obtenerValoresMayoresA", "obtenerValoresMenoresA","validarBody"])
+        _.bindAll(this, ["escribirValor", "obtenerValorDeClave", "obtenerValoresMayoresA", "obtenerValoresMenoresA", "validarBody"])
     }
 
-    obtenerValorDeClave({ params: { key } }, response) {
-        let valor = this.repositorioDeDatos.obtenerValor(key);
-        if (valor) {
-            response.json({ valor });
-        }
-        else {
-            response.sendStatus(404);
-        }
+    obtenerValorDeClave({ params: { key } }, res) {
+        return Promise.resolve(this.repositorioDeDatos.obtenerValor(key))
+            .tap((valor) => { if (_.isUndefined(valor)) throw new NotFound() })
+            .then((valor) => { valor });
     }
 
     escribirValor({ body }, res) {
         return Promise.method(this.validarBody)(body)
-            .then(this.repositorioDeDatos.escribirValor(body))
+            .then(() => this.repositorioDeDatos.escribirValor(body))
     }
 
     bodyValido(par) {
