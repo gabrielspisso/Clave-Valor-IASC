9f0454c4a9cc49ced2a5188abb2200e6088769e9
Diego <diegoperezferrando@gmail.com>
2019-07-14 22:10:59 -0300

    Fix spaces

--- Orquestador/api/model/orquestador.js
+++ Orquestador/api/model/orquestador.js
@@ -4,29 +4,29 @@ const config = require("../../config");
 
 class Orquestador {
 
-    constructor() {
-        this.nodes = config.nodes.map(it => new Node(it));
-        this.isMaster = false;
-    }
-    
-    getValue(key) {
-        return Promise.any(this.nodes, it => it.getByKey(key))
-            .then(({ valor }) => { key, value: valor });
-    }
+  constructor() {
+    this.nodes = config.nodes.map(it => new Node(it));
+    this.isMaster = false;
+  }
+  
+  getValue(key) {
+    return Promise.any(this.nodes, it => it.getByKey(key))
+      .then(({ valor }) => { key, value: valor });
+  }
 
-    getHighThan(value) {
-        return this._getRangeBy(it => it.getHigherThan(value));
-    }
+  getHighThan(value) {
+    return this._getRangeBy(it => it.getHigherThan(value));
+  }
 
-    getLessThan(value) {
-        return this._getRangeBy(it => it.getLesserThan(value));
-    }
+  getLessThan(value) {
+    return this._getRangeBy(it => it.getLesserThan(value));
+  }
 
-    _getRangeBy(getter) {
-        return Promise.map(this.nodes, getter)
-            .then(_.flatten);
-    }
-    
+  _getRangeBy(getter) {
+    return Promise.map(this.nodes, getter)
+      .then(_.flatten);
+  }
+  
 }
 
 module.exports = Orquestador;
