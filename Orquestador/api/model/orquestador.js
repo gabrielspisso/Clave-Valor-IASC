80863a591155e8c145a24bf12a2f825be627a9c7
lclauser <lclauser@est.frba.utn.edu.ar>
2019-07-14 21:56:33 -0300

    getValue

--- Orquestador/api/model/orquestador.js
+++ Orquestador/api/model/orquestador.js
@@ -1,66 +1,17 @@
 const _ = require("lodash");
 const Promise = require("bluebird");
-const request = require("request-promise");
 const config = require("../../config");
-const NodeSelector = require("./nodeSelector");
 
-class Orquestador{
-    constructor(){
-        this.nodes = _.map(config.nodes, function(dir){ 
-            return { 
-                ip: dir, 
-                active: true 
-            } 
-        });
-        this.isMaster=false;
-        this.ns = new NodeSelector(this.nodes);
+class Orquestador {
+
+    constructor() {
+        this.nodes = config.nodes.map(it => new Node(it));
+        this.isMaster = false;
     }
     
-    _requestNode(node, options) {
-        return request({ 
-            uri: `${node.ip}/${options.resource}`,
-            timeout: 10000,
-            ...options
-        })
-        .catch(function (err) {
-            node.active=false;
-            return err;
-        });
-    }
-    
-    getValue(key){
-        var node = ns.getNodeByKey(key);
-        return this._requestNode(node,{
-            method: "GET",
-            resource: key
-          });
-    }
-
-    assignKeyAndValue(body){
-        var node = ns.getNodeByKey(body.key);
-        return this._requestNode(node,{
-            method: "GET",
-            resource: "",
-            json: body
-          });
-    }
-
-    getValuesByCondition(condition,value){
-        return Promise.map(this.nodes, function(node){ 
-            this._requestNode(node, {
-                method: "GET",
-                resource: condition+"/"+value
-            });
-        })
-        .then(_.flatten);
-    }
-
-    getHighThan(value){
-        return getValuesByCondition("mayor",value);
-    }
-
-    getLessThan(value){
-        return getValuesByCondition("menor",value);
+    getValue(key) {
+        return Promise.any(this.nodes, it => it.getByKey(key))
+            .then(({ valor }) => { key, value: valor });
     }
     
 }
