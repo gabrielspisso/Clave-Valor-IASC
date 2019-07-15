43ea2eb942544eb31241975ae4986508d28e1fde
Diego <diegoperezferrando@gmail.com>
2019-07-14 22:43:21 -0300

    Middleware para que paredes deje de llorar

--- Orquestador/api/routes.js
+++ Orquestador/api/routes.js
@@ -3,11 +3,13 @@ const controller = require("./controller");
 const PORT = process.env.PORT || 9000;
 const { route } = require("endpoint-handler")(app);
 
+
+route.get('/esMaster', controller.esMaster);
+app.use((req, res, next) => controller.esMaster() ? next() : res.sendStatus(400));
 route.get('/:key', controller.obtenerValor);
 route.get('/mayor', controller.mayorA)
 route.get('/menor', controller.menorA);
 route.post('/crearValor', controller.crearValor);
-route.get('/esMaster', controller.esMaster);
 
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
